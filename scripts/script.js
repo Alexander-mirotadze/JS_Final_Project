19cc193a67c87f463fd08f75a9e559c0fd75737b
Alexander-mirotadze <145982962+Alexander-mirotadze@users.noreply.github.com>
2024-03-10 00:05:56 +0400

    burger-animation-on-open-but-not-on-close

--- scripts/script.js
+++ scripts/script.js
@@ -174,6 +174,14 @@ const navLists = document.querySelector(".nav");
 
 burgerBar.addEventListener("click", function(){
   navLists.classList.toggle("active-nav");
+
+  const burgerMiddleLine = document.querySelector(".burger__middle-line");
+  burgerMiddleLine.classList.toggle("burger__middle-line-hide")
+
+  const burgerBarLines = document.querySelectorAll(".burger__lines");
+  burgerBarLines.forEach(function (element) {
+    element.classList.toggle("burger__lines-to-X")
+  })
 })
 
 // ! scroll up
